a8df2ffade3f5b4b852cbf1434f8eafd9a3bdb2f
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-07 16:15:15 +0000

    cleaned up whitespace

--- dice.rb
+++ dice.rb
@@ -11,10 +11,6 @@ get("/") do
   erb(:elephant)
 end
 
-get("/") do
-  "Hello World"
-end
-
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
@@ -24,45 +20,57 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+	end
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  2.times do
+    die = rand(1..10)
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+    @rolls.push(die)
+  end
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-  outcome = "You rolled a #{first_roll}."
+  @rolls = []
 
-  "<h1>1d20</h>
-  <p>#{outcome}</p>"
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+    @rolls.push(die)
+  end
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
